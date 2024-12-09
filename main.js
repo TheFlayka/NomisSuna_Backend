b78d1b904212b71f7388e6972488e6107f6645f9
Flayka <the.flayka@gmail.com>
2024-12-09 10:35:42 +0500

    LocalHost will be HTTPS

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3405
 const corsOptions = {
-	origin: 'http://localhost:5173',
+	origin: 'https://localhost:5173',
 	credentials: true, 
 }
 
