1902d53c5ba34ac1fd0c0cca056eca39bde78768
Flayka <the.flayka@gmail.com>
2024-12-15 12:30:28 +0500

    delete options

--- main.js
+++ main.js
@@ -17,7 +17,6 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(cookieParser())
-app.options('*', cors(corsOptions))
 app.use(express.json())
 app.use(userRoutes)
 app.use(tokenRoutes)
